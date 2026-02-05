5ce2fd286860518faeeeceffb48b062017346401
Allan <allandebert@gmail.com>
2026-02-05 14:55:52 +0100

    chore: Part_1 compliant with Flake8 norm

--- leaffliction/dataset.py
+++ leaffliction/dataset.py
@@ -8,43 +8,43 @@ import constants as consts
 
 
 def downloadZip():
-	if os.path.exists("dataset.zip"):
-		os.remove("dataset.zip")
-	try:
-		print("Downloading Dataset Zip")
-		urllib.request.urlretrieve(consts.DATASET_URL, "dataset.zip")
-		print("Download Completed")
-		unzipData()
-	except HTTPError as e:
-		print("Error: HTTP Error")
-	except URLError as e:
-		print("Error: URL Error")
-	except Exception as e:
-		print("Error: Unowkn Error")
+    if os.path.exists("dataset.zip"):
+        os.remove("dataset.zip")
+    try:
+        print("Downloading Dataset Zip")
+        urllib.request.urlretrieve(consts.DATASET_URL, "dataset.zip")
+        print("Download Completed")
+        unzipData()
+    except HTTPError as e:
+        print("Error: HTTP Error")
+    except URLError as e:
+        print("Error: URL Error")
+    except Exception as e:
+        print("Error: Unowkn Error")
 
 def unzipData():
-	try:
-		with zipfile.ZipFile("dataset.zip", "r") as z:
-			if z.testzip() is not None:
-				print("Zip file is Corrupted")
-			else:
-				z.extractall()
-				print("Unzip Successful")
-				replaceData()
-	except zipfile.BadZipFile:
-		print("Error: The file is not a zip file or is corrupted.")
-	except FileNotFoundError:
-		print("Error: The zip file was not found.")
-	except PermissionError:
-		print("Error: You don't have permission to write to that folder.")
-	except Exception as e:
-		print(f"An unexpected error occurred: {e}")
+    try:
+        with zipfile.ZipFile("dataset.zip", "r") as z:
+            if z.testzip() is not None:
+                print("Zip file is Corrupted")
+            else:
+                z.extractall()
+                print("Unzip Successful")
+                replaceData()
+    except zipfile.BadZipFile:
+        print("Error: The file is not a zip file or is corrupted.")
+    except FileNotFoundError:
+        print("Error: The zip file was not found.")
+    except PermissionError:
+        print("Error: You don't have permission to write to that folder.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 def replaceData():
-	if os.path.exists("dataset"):
-		shutil.rmtree("dataset")
-		print("Old data removed")
-	os.rename("images", "dataset")
-	os.remove("dataset.zip")
+    if os.path.exists("dataset"):
+        shutil.rmtree("dataset")
+        print("Old data removed")
+    os.rename("images", "dataset")
+    os.remove("dataset.zip")
 
 downloadZip()
